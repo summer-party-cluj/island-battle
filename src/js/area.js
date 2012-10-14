cf77619f9946e1a2277064b224c7ce59e0e5f2c3
Paul Comanici <darkyndy@gmail.com>
2012-10-14 19:01:54 +0300

    - rescale;

--- src/js/area.js
+++ src/js/area.js
@@ -1,7 +1,10 @@
 (function () {
 	var my = {
-		battleWidth: 200,
-		battleHeight: 400,
+		battleWidth: 640,
+		battleHeight: 360,
+		useBattleWidth: 0,
+		useBattleHeight: 0,
+		isLandscape: true,
 		areaId: "playArea",
 		battleId: "battleArea",
 		eventsSet: false,
@@ -16,6 +19,26 @@
 			var areaEl;
 			areaEl = document.getElementById(my.battleId);
 			return areaEl;
+		},
+		prepareBattleAreaDim: function (topDim, leftDim, w, h, isLandscape) {
+			var battleEl;
+			topDim = topDim / 2;
+			topDim = parseInt(topDim, 10);
+			leftDim = leftDim / 2;
+			leftDim = parseInt(leftDim, 10);
+			battleEl = my.getBattleAreaElement();
+			battleEl.style["width"] = w + "px";
+			battleEl.style["height"] = h + "px";
+			battleEl.style.top = topDim + "px";
+			battleEl.style.left = leftDim + "px";
+			battleEl.style.webkitTransformOrigin = parseInt(h / 2, 10) + "px";
+			battleEl.style.mozTransformOrigin = parseInt(h / 2, 10) + "px";
+			battleEl.style.oTransformOrigin = parseInt(h / 2, 10) + "px";
+			battleEl.style.transformOrigin = parseInt(h / 2, 10) + "px";
+			battleEl.classList.remove("portrait");
+			if (!isLandscape) {
+				battleEl.classList.add("portrait");
+			}
 		},
 		prepareBattleArea: function (w, h) {
 			/**
@@ -33,7 +56,8 @@
 				dimensionNameBigger = "width",
 				dimensionNameSmaller = "height",
 				isLandscape = true,
-				battleEl;
+				battleEl,
+				scaleRatio;
 			maxDim = w;
 			otherDim = h;
 			if (maxDim < h) {
@@ -49,34 +73,26 @@
 				baseDim = baseDim2;
 			}
 			battleBiggerDim = 16 * baseDim;
+			scaleRatio = battleBiggerDim / my.battleWidth;
 			battleSmallerDim = 9 * baseDim;
+			my.useBattleWidth = battleBiggerDim;
+			my.useBattleHeight = battleSmallerDim;
 			if (isLandscape) {
 				topDim = h - battleSmallerDim;
-				my.battleWidth = battleBiggerDim;
-				my.battleHeight = battleSmallerDim;
+				leftDim = w - battleBiggerDim;
 			} else {
 				topDim = h - battleBiggerDim;
-				my.battleWidth = battleSmallerDim;
-				my.battleHeight = battleBiggerDim;
+				leftDim = w - battleSmallerDim;
 			}
-			topDim = topDim / 2;
-			topDim = parseInt(topDim, 10);
-			battleEl = my.getBattleAreaElement();
-			battleEl.style[dimensionNameBigger] = battleBiggerDim + "px";
-			battleEl.style[dimensionNameSmaller] = battleSmallerDim + "px";
-			battleEl.style.top = topDim + "px";
+			my.prepareBattleAreaDim(topDim, leftDim, battleBiggerDim, battleSmallerDim, isLandscape);
+			IB.Island.update(scaleRatio);
 		},
-		delegate: function () {
-			/**
-			 * @description Attach events
-			 */
-			window.addEventListener("resize", my.init, false);
-		},
-		init: function () {
+		init: function (forUpdate) {
 			var windowScreenWidth,
 				windowScreenHeight,
 				areaEl,
 				mainEl;
+			forUpdate = forUpdate || false;
 			mainEl = document.querySelector("html");
 			windowScreenWidth = mainEl.clientWidth;
 			windowScreenHeight = mainEl.clientHeight;
@@ -84,15 +100,27 @@
 			areaEl.style.width = windowScreenWidth + "px";
 			areaEl.style.height = windowScreenHeight + "px";
 			//prepare battle area
-			my.prepareBattleArea(windowScreenWidth, windowScreenHeight);
+			if (!forUpdate) {
+				my.prepareBattleAreaDim(0, 0, my.battleWidth, my.battleHeight, true, 1);
+			}
 			areaEl.style.display = "block";
 			if (!my.eventsSet) {
 				my.eventsSet = true;
 				my.delegate();
 				IB.Island.init(my.battleWidth, my.battleHeight);
 			} else {
-				IB.Island.update(my.battleWidth, my.battleHeight);
+				IB.Island.update(my.useBattleWidth, my.useBattleHeight);
 			}
+			my.prepareBattleArea(windowScreenWidth, windowScreenHeight);
+		},
+		update: function () {
+			my.init(true);
+		},
+		delegate: function () {
+			/**
+			 * @description Attach events
+			 */
+			window.addEventListener("resize", my.update, false);
 		}
 	},
 	Area = (function () {
--- src/js/island.js
+++ src/js/island.js
@@ -4,69 +4,125 @@
 		h: 0,
 		cX: [],
 		cY: [],
-		cR: 20,
+		cR: 48,
+		minRad: 16,
+		maxRad: 50,
 		inslandsNr: 10,
+		islands: [],
+		ratioDraw: 1,
+		generateRandomDim: function () {
+			return Math.round(my.minRad + Math.random() * (my.maxRad - my.minRad));
+		},
+		calculateIslandCoords: function (n) {
+			my.cX[n] = Math.round((Math.random() * (my.w - my.cR * 2))) + my.cR;
+			my.cY[n] = Math.round((Math.random() * (my.h - my.cR * 2))) + my.cR;
+		},
 		measureDistance: function (n) {
 			var i,
 				dX,
 				dY,
 				distanceQ,
-				dMinQ = Math.pow(my.cR*2,2),
-				cvW = 800,
-		        cvH = 400;
+				dMinQ = Math.pow(my.cR * 2, 2);
 			dMinQ = Math.pow(my.cR * 2, 2);
 			for (i = 0; i < n; i = i + 1) {
                 dX = my.cX[n] - my.cX[i];
                 dY = my.cY[n] - my.cY[i];
-                distanceQ = Math.round(Math.pow(dX,2)+Math.pow(dY,2));
+                distanceQ = Math.round(Math.pow(dX, 2) + Math.pow(dY, 2));
                 if (distanceQ < dMinQ) {        
-                	my.cX[n] = Math.round((Math.random() * (cvW - my.cR*2))) + my.cR;
-                	my.cY[n] = Math.round((Math.random() * (cvH - my.cR*2))) + my.cR;
+                	my.calculateIslandCoords(n);
                     i = 0;                    
                     dX = my.cX[n] - my.cX[i];
                     dY = my.cY[n] - my.cY[i];
-                    distanceQ = Math.round(Math.pow(dX,2)+Math.pow(dY,2));
+                    distanceQ = Math.round(Math.pow(dX, 2) + Math.pow(dY, 2));
                     i = i - 1;
                 }
             }
 		},
 		buildIslands: function () {
-			var islandEl,
-				battleEl;
+			var islandOuterEl,
+				islandEl,
+				battleEl,
+				islandRad,
+				islandOwner,
+				inslandsNr;
 			battleEl = IB.Area.getBattleAreaElement();
-			for (i = 0; i < my.inslandsNr ;i = i + 1) {
-				my.cX[i] = Math.round((Math.random() * (my.w-my.cR*2))) + my.cR;
-				my.cY[i] = Math.round((Math.random() * (my.h-my.cR*2))) + my.cR;
+			inslandsNr = my.inslandsNr;
+			for (i = 0; i < inslandsNr ; i = i + 1) {
+				islandOwner = 0;
+				if (i === 0) {
+					islandOwner = 1;
+				} else if (i === 1) {
+					islandOwner = -1;
+				}
+				if (i > 2) {
+					my.cR = my.generateRandomDim();
+					if (my.cR % 2 === 1) {
+						my.cR = my.cR - 1;
+					}
+				}
+				my.calculateIslandCoords(i);
 				if (i > 0) {
 					my.measureDistance(i);
 				}
+				islandOuterEl = document.createElement("DIV");
+				islandOuterEl.setAttribute("class", "islandOuter");
+				islandOuterEl.setAttribute("id", "island" + i);
 				islandEl = document.createElement("DIV");
-				islandEl.setAttribute("class", "island");
-				islandEl.style.top = (my.cY[i] - my.cR) + "px";
-				islandEl.style.left = (my.cX[i] - my.cR) + "px";
-				islandEl.style.width = 2 *my.cR + "px";
-				islandEl.style.height = 2 *my.cR + "px";
-				islandEl.style.borderRadius = my.cR + "px";
-				islandEl.innerHTML = my.cR;
-				battleEl.appendChild(islandEl);
+				islandEl.setAttribute("class", "island owner" + islandOwner);
+				islandOuterEl.appendChild(islandEl);
+				battleEl.appendChild(islandOuterEl);
+				my.islands[i] = {
+					owner: islandOwner,
+					rad: my.cR,
+					top: (my.cY[i] - my.cR),
+					left: (my.cX[i] - my.cR),
+					initVal: (my.cR  / 2),
+					val: (my.cR  / 2)
+				};
 			}
+		},
+		getLoc: function () {
+			return my.islands;
 		},
 		init: function (w, h) {
 			my.w = w;
 			my.h = h;
 			my.buildIslands();
-			//console.log(my.cX);
-			//console.log(my.cY);
 		},
-		update: function (w, h) {
-			my.w = w;
-			my.h = h;
+		update: function (ratio) {
+			var islandOuterEl,
+				islandEl,
+				islandRad,
+				thisIsland,
+				radius,
+				islandsNr;
+			ratio = ratio || 1;
+			inslandsNr = my.inslandsNr;
+			for (i = 0 ; i < my.inslandsNr; i = i + 1) {
+				thisIsland = my.islands[i];
+				radius = parseInt(thisIsland.initVal * ratio, 10);
+				islandOuterEl = document.getElementById("island" + i);
+				islandOuterEl.style.top = parseInt(thisIsland.top * ratio, 10) + "px";
+				islandOuterEl.style.left = parseInt(thisIsland.left * ratio, 10) + "px";
+				islandOuterEl.style.width = radius * 2 + "px";
+				islandOuterEl.style.height = radius * 2+ "px";
+				islandOuterEl.style.borderRadius = radius * 2+ "px";
+				islandRad = radius - 1;
+				//now island
+				islandEl = islandOuterEl.childNodes[0];
+				islandEl.style.lineHeight = islandRad * 2 + "px";
+				islandEl.style.width = islandRad * 2 + "px";
+				islandEl.style.height = islandRad * 2 + "px";
+				islandEl.style.borderRadius = islandRad * 2 + "px";
+				islandEl.innerHTML = thisIsland.val;
+			}
 		}
 	},
 	Island = (function () {
 		return {
 			init: my.init,
-			update: my.update
+			update: my.update,
+			getLoc: my.getLoc
 		};
 	}());
 	window.IB.Island = Island;
